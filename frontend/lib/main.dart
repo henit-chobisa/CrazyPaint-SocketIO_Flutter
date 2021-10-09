f94994d0453aecfd100ab2567e9fcd1725ccc01f
Henit Chobisa <henitchobisa0885@gmail.com>
2021-10-09 15:01:24 +0530

    banner page completed

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/logoPage.dart';
 import 'package:frontend/paintPage.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class CrazyPaint extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: paintPage(),
+      home: LogoPage(),
     );
   }
 }
