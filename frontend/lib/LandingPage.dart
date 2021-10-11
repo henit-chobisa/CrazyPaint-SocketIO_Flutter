79bfceb6fd5f0da0cc27488e1fbab11c57effadc
Henit Chobisa <henitchobisa0885@gmail.com>
2021-10-11 18:48:49 +0530

    Project edge completion

--- frontend/lib/LandingPage.dart
+++ frontend/lib/LandingPage.dart
@@ -36,144 +36,146 @@ class _LandingPageState extends State<LandingPage> {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 70.h,
-                  ),
-                  Center(
-                    child: Text(
-                      "Crazy Paint",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 50.sp,
-                          fontWeight: FontWeight.bold),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 70.h,
                     ),
-                  ),
-                  Center(
-                    child: Text(
-                      'Developed and Designed by Henit Chobisa',
-                      style: TextStyle(color: Colors.white, fontSize: 14.sp),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30.h,
-                  ),
-                  Lottie.asset('Lottie/cube.json'),
-                  customTextFeild(
-                    hintText: "Enter Room ID to join",
-                    obsText: false,
-                    controller: controller,
-                    inputType: TextInputType.text,
-                  ),
-                  SizedBox(
-                    height: 8.h,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          if (controller.text.isNotEmpty) {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (_) => paintPage(
-                                          roomID: controller.text,
-                                        )));
-                          }
-                        },
-                        child: Container(
-                          height: 50.h,
-                          width: 180.w,
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(20.r)),
-                          child: Center(
-                            child: Text(
-                              "Join",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 17.sp,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 20.w,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          controller.text = getRandomString(20);
-                        },
-                        child: Container(
-                          height: 50.h,
-                          width: 180.w,
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(20.r)),
-                          child: Center(
-                            child: Text(
-                              "Generate",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 17.sp,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30.h,
-                  ),
-                ],
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.grey.shade900,
-                    borderRadius: BorderRadius.circular(20.r)),
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      top: 8.h, bottom: 8.h, left: 16.w, right: 16.w),
-                  child: Row(
-                    children: [
-                      Text(
-                        auth.currentUser.displayName,
+                    Center(
+                      child: Text(
+                        "Crazy Paint",
                         style: TextStyle(
                             color: Colors.white,
-                            fontSize: 17.sp,
+                            fontSize: 50.sp,
                             fontWeight: FontWeight.bold),
                       ),
-                      Spacer(),
-                      Container(
-                        height: 40.h,
-                        width: 40.w,
-                        decoration: BoxDecoration(
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(20.r)),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20.r),
-                          child: Image(
-                            image: NetworkImage(auth.currentUser.photoURL),
-                            fit: BoxFit.cover,
+                    ),
+                    Center(
+                      child: Text(
+                        'Developed and Designed by Henit Chobisa',
+                        style: TextStyle(color: Colors.white, fontSize: 14.sp),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.h,
+                    ),
+                    Lottie.asset('Lottie/cube.json'),
+                    customTextFeild(
+                      hintText: "Enter Room ID to join",
+                      obsText: false,
+                      controller: controller,
+                      inputType: TextInputType.text,
+                    ),
+                    SizedBox(
+                      height: 8.h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            if (controller.text.isNotEmpty) {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) => paintPage(
+                                            roomID: controller.text,
+                                          )));
+                            }
+                          },
+                          child: Container(
+                            height: 50.h,
+                            width: 180.w,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20.r)),
+                            child: Center(
+                              child: Text(
+                                "Join",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 17.sp,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
                           ),
                         ),
-                      )
-                    ],
+                        SizedBox(
+                          width: 20.w,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            controller.text = getRandomString(20);
+                          },
+                          child: Container(
+                            height: 50.h,
+                            width: 180.w,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20.r)),
+                            child: Center(
+                              child: Text(
+                                "Generate",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 17.sp,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30.h,
+                    ),
+                  ],
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade900,
+                      borderRadius: BorderRadius.circular(20.r)),
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        top: 8.h, bottom: 8.h, left: 16.w, right: 16.w),
+                    child: Row(
+                      children: [
+                        Text(
+                          auth.currentUser.displayName,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 17.sp,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Spacer(),
+                        Container(
+                          height: 40.h,
+                          width: 40.w,
+                          decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(20.r)),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20.r),
+                            child: Image(
+                              image: NetworkImage(auth.currentUser.photoURL),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
